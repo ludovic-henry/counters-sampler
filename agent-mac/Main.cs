53973039e7c8edee88d4e22964f6e28c12b78089
Ludovic Henry <ludovic.henry@xamarin.com>
2014-05-19 09:12:40 -0400

    [agent-mac] Creation

---
+++ agent-mac/Main.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Net;
+using System.Collections.Generic;
+using MonoCounters.Common.Agent;
+
+namespace MonoCounters.Agent.Mac
+{
+	public class MainClass
+	{
+
+
+		public static void Main (string[] args)
+		{
+			if (args.Length < 3)
+				throw new ArgumentException ("usage : root samples_file inspector_address");
+
+			var root = args [0];
+			var samplesFile = Path.IsPathRooted (args [1]) ? args [1] : Path.Combine (root, args [1]);
+			var inspectorAddress = args [2];
+
+			var arguments = Arguments.Parse (File.Open (Path.Combine (
+				root, "arguments.ini"), FileMode.Open));
+
+			var assembly = Path.IsPathRooted (arguments.Assembly) ?
+				arguments.Assembly : Path.Combine (root, arguments.Assembly);
+
+			var time = new Benchmark (Assembly.LoadFile (assembly), arguments.Class)
+				.Run (arguments.BenchmarkArguments.ToArray ());
+
+			using (var samples = File.Open (samplesFile, FileMode.Open)) {
+				new ResultUploader (inspectorAddress).Upload (1, new FileStream (samplesFile, FileMode.Open));
+			}
+
+			Console.WriteLine ("{0:N3} ms", time / 10000d);
+		}
+	}
+}
