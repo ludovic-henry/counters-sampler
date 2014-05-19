7a1ec2afb9be10a73d97909a4ad7a5a99376b176
Ludovic Henry <ludovic.henry@xamarin.com>
2014-05-19 08:54:48 -0400

    [agent-android-test] Create test package to run on Test Cloud

---
+++ agent-android-tests/BenchmarkTestFixture.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Android;
+using MonoCounters.Common.Agent;
+using System.IO;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace agentandroidtests
+{
+	[TestFixture]
+	public class BenchmarkTestFixture
+	{
+		AndroidApp app;
+
+		string AppFile = "agent_android.agent_android-Signed.apk";
+
+		[SetUp]
+		public void SetUp ()
+		{
+			app = ConfigureApp
+				.Android
+				.ApkFile (AppFile)
+				.StartApp ();
+		}
+
+		[Test]
+		public void WaitForNoBenchmarkImageTest ()
+		{
+			app.WaitForNoElement (c => c.Id ("BenchmarkImage"));
+		}
+	}
+}
+
